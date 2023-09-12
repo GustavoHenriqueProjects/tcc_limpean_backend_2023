fe75aa63c49976229fa8f618210516e21a69a169
Leticia Evelin <ljkeleticia@gmail.com>
2023-09-12 19:13:14 -0300

    endpoint delete cliente

--- src/server/routes/router.ts
+++ src/server/routes/router.ts
@@ -1,20 +1,20 @@
-//import { loginTypeUser } from "../../controller/controllerUser/login/loginTypeUser"
+//import { loginClient } from "../../controller/controllerCliente/loginCliente/controllerLogin"
+//import { loginDiarist } from "../../controller/controllerDiarista/loginDiarista/controllerLogin"
 
 import { Router, Request, Response, NextFunction } from "express"
 import bodyParser from 'body-parser'
-import { loginClient } from "../../controller/controllerCliente/loginCliente/controllerLogin"
-import { loginDiarist } from "../../controller/controllerDiarista/loginDiarista/controllerLogin"
+import { loginTypeUser } from "../../controller/controllerUser/login/loginTypeUser"
 import { registerTypeUser } from "../../controller/controllerUser/register/registerTypeUser"
 import { dataDiaristById } from "../../controller/controllerDiarista/dataDiarist/controllerDataDiaristById"
 import { dataAllDiarist } from "../../controller/controllerDiarista/dataDiarist/controllerDataAllDiarist"
+import { deleteRegisterClient } from "../../controller/controllerCliente/deleteRegisterClient/controllerDeleteRegisterClient"
 import * as message from "../../modulo/config"
-import * as jwt from 'jsonwebtoken'
+import * as jwt  from "jsonwebtoken"
 
 const jsonParser = bodyParser.json()
 
 const router = Router()
 
-//Função para verifica token
 const verifyJWT = async function(request: Request, response: Response, next: NextFunction) {
     //Pra uso no Postman
     const token = request.headers['x-api-key'];    
@@ -30,11 +30,12 @@ const verifyJWT = async function(request: Request, response: Response, next: Nex
     }
 
     try {
-        //Discriptografa token 
-        const decoded = jwt.verify(Array.isArray(token) ? token[0] : token, SECRETE);
+
+        jwt.verify(Array.isArray(token) ? token[0] : token, SECRETE)
+
         next();
     } catch (error) {
-        
+                
         return response.status(401).json(message.ERRO_INVALID_TOKEN)
     }
 }
@@ -62,10 +63,8 @@ router.post('/v1/limpean/cadastro', jsonParser, async function (request: Request
     }
 })
 
-//****************************************Cliente*****************************************************
-
-//Endpoint responsavel por realizar a validação do login client
-router.post('/v1/limpean/client/login', jsonParser, async function (request, response) {
+/***************************************** Login Cliente e Diarista ***********************************/
+router.post('/v1/limpean/login', jsonParser, async function (request, response) {
 
     let contentType = request.headers['content-type']
 
@@ -73,60 +72,72 @@ router.post('/v1/limpean/client/login', jsonParser, async function (request, res
         
         let dataBody = request.body
         
-        let status = await loginClient(dataBody)
+        let statusLogin = await loginTypeUser(dataBody)
 
-        if(status){
-            response.status(status.status)
-            response.json(status)
+        if(statusLogin){
+            response.status(statusLogin.status)
+            response.json(statusLogin)
         }else{
             response.send(message.ERRO_INTERNAL_SERVER)
         }
     }
 })
 
+//EndPoint para excluir um cliente
+router.delete('/v1/limpean/client/:token', verifyJWT, async function (request, response){
+
+    const token = request.params.token
+    const statusClient = await deleteRegisterClient(token)
+
+    response.status(statusClient.status)
+    response.json(statusClient)
+
+})
+//****************************************Cliente*****************************************************
+
+//Endpoint responsavel por realizar a validação do login client
+// router.post('/v1/limpean/client/login', jsonParser, async function (request, response) {
+
+//     let contentType = request.headers['content-type']
+
+//     if(contentType === 'application/json'){
+        
+//         let dataBody = request.body
+        
+//         let status = await loginClient(dataBody)
+
+//         if(status){
+//             response.status(status.status)
+//             response.json(status)
+//         }else{
+//             response.send(message.ERRO_INTERNAL_SERVER)
+//         }
+//     }
+// })
+
 /******************** Diarist ****************************************************************/
 
 //Endpoint responsavel por realizar a validação do diarista
-router.post('/v1/limpean/diarist/login', jsonParser, async function (request, response) {
+// router.post('/v1/limpean/diarist/login', jsonParser, async function (request, response) {
 
-    let contentType = request.headers['content-type']
-
-    if(contentType === 'application/json'){
-        
-        let dataBody = request.body
-        
-        let status = await loginDiarist(dataBody)
-
-        if(status){
-            response.status(status.status)
-            response.json(status)
-        }
-        else{
-            response.send(message.ERRO_INTERNAL_SERVER)
-        }
-    }
-})
-
-
-//EndPoint responsavel por deletar o cadastro do cliente
-// router.delete('/v1/cadastro/cliente', jsonParser, async function (request: Request, response: Response) {
-    
 //     let contentType = request.headers['content-type']
 
-//     if (contentType === 'application/json') {
-
+//     if(contentType === 'application/json'){
+        
 //         let dataBody = request.body
+        
+//         let status = await loginDiarist(dataBody)
 
-//         let status = await controllerCliente.deleteRegisterCliente(dataBody)
-    
-//         response.status(status.status)
-//         response.json(status)
-        
-        
-//     } else {
-//         return response.send(message.ERROR_INVALID_CONTENT_TYPE)
+//         if(status){
+//             response.status(status.status)
+//             response.json(status)
+//         }
+//         else{
+//             response.send(message.ERRO_INTERNAL_SERVER)
+//         }
 //     }
 // })
+
 
 //EndPoint para listar todos os diaristas
 router.get('/v1/limpean/diarist', async function (request, response) {
@@ -137,6 +148,17 @@ router.get('/v1/limpean/diarist', async function (request, response) {
     response.json(statusDataDiarist)
     
 })
+
+// router.delete('/v1/limpean/diarist/:token', verifyJWT, async function (request, response){
+
+//     const token = request.params.token
+//     const statusDiarist = await deleteRegisterDiarist(token)
+
+//     response.status(statusDiarist.status)
+//     response.json(statusDiarist)
+
+// })
+
 
 //EndPoint para listar o diarista com base no id
 router.get('/v1/limpean/diarist/:token', verifyJWT, async function (request, response) {
